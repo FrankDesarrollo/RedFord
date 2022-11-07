cb8657a00f36e0d478675e70758a16cbf5c1a476
josearpa123 <116037142+josearpa123@users.noreply.github.com>
2022-11-06 20:53:54 -0500

    Add files via upload

---
+++ bienvenido.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a RedFord</title>
+    <!-- FONT AWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <!-- FONT OSWALD -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+
+    
+    <!-- CUSTOM CSS-->
+    <link rel="stylesheet" href="style.css">
+
+
+
+</head>
+<body>
+
+    <div class="menu-btn">
+        <i class="fas fa-bars fa-2x"></i>
+      </div>
+    
+      <div class="container">
+        <!-- Navigation -->
+        <nav class="nav-main">
+          <!-- Brand -->
+          <img src="img/REDFORD1.png" alt="RedFord Logo" class="nav-brand">
+          <!-- Left Nav -->
+          <ul class="nav-menu">
+            <li>
+              <a href="#">Noticias</a>
+            </li>
+            <li>
+              <a href="#">Sobre Nosotros</a>
+            </li>
+            <li>
+              <a href="#">Podcast & Opiniones</a>
+            </li>
+            <li>
+              <a href="#">Desarrollo</a>
+            </li>
+            <li>
+              <a href="index.php">cerrar sesion</a>
+            </li>
+            <li>
+              <a href="#">Cryptocurrencies</a>
+            </li>
+            <li>
+              <a href="#">More</a>
+            </li>
+          </ul>
+    
+          <!-- Right Nav -->
+          <ul class="nav-menu-right">
+            <li>
+              <a href="#">
+                <i class="fas fa-search"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <hr>
+
+        <!-- SHOWCASE-->
+        <!-- Banner -->
+        <header class="showcase">
+            <h2>Red Ford INT</h2>
+            <p>Podcast & Opiniones</p>
+            <a href="#"class="btn";>Leer Mas <i class="fas fa-angle-double-right"></i></a>
+        </header>
+
+        <!-- Noticias del Podcast Columnas-->
+        <div class="news-carsd">
+            <div>
+                <img src="img/1.jpg" alt="News 1">
+                <h3 style="color: black;">Jose David Arias</h3>
+                <p style="color:black;">mi papel ha sido como un desarrollador front-end he trabajado con mis compañeros en la interfaz de usuario desde el punto de vista del código, para que la interacción con el sistema sea posible. Por lo tanto, me he encargado de la parte visual de la página web (de todo aquello que es visible en la página web) haciendo que su diseño sea intuitivo y atractivo esto se logró interactuando con la información, pero de una forma digital usando HTML, CSS, JavaScript..</p>
+                <a href="#" style="color:black;">Leer Mas <i class="fas fa-angle-double-right"></i></a>
+
+            </div>
+            <div>
+                <img src="img/2.jpg" alt="News 1">
+                <h3 style="color: black;">Jeison Mauricio Nazarit</h3>
+                <p style="color:black;">Al igual que mis compañeros he apoyado en cuanto a la parte de programación, el código de la página que hemos estado adelantando, adicional al ello, también he sido quien ha liderado la parte de los diseños de los logos, todo lo que es la multimedia, que se han implementado a la página, que una vez, terminada, podrán visualizar..</p>
+                <a href="#" style="color:black;">Leer Mas <i class="fas fa-angle-double-right"></i></a>
+
+            </div>
+            <div>
+                <img src="img/3.jpg" alt="News 1">
+                <h3 style="color: black;">Erick Collin Albornoz</h3>
+                <p style="color:black;">Encargado de planificar e organizar el planteamiento laboral de la página, incluyendo el patrón que tenemos como grupo en el software, mostrando evidencia que la información obtenida por nosotros sea verídica, responsable a la hora de garantizar el funcionamiento correcto o el buen uso de herramientas para el sitio web, mostrar novedades o actualizaciones del servicio (Noticias, ya sean escritas o de carácter auditivo), seguimiento del diseño, estilo y estructura para facilitar el uso del contenido que ofrecemos como tal. (Multimedia, gráficas y animaciones)</p>
+                <a href="#" style="color:black;">Leer Mas <i class="fas fa-angle-double-right"></i></a>
+
+            </div>
+            <div>
+                <img src="img/4.jpg" alt="News 1">
+                <h3 style="color: black;">Frank David Palma</h3>
+                <p style="color:black;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod ab culpa dolorum nemo excepturi! Fugiat quas obcaecati odio! Tempore reiciendis velit beatae facilis aliquam consequuntur voluptates, culpa perspiciatis voluptas enim.</p>
+                <a href="#" style="color:black;">Leer Mas <i class="fas fa-angle-double-right"></i></a>
+
+            </div>
+
+        </div>
+
+        <section class="cards-banner-one">
+          <div class="content">
+            <h2 style="color: black;">Vision</h2>
+            <p style="color: aliceblue;">Concientizar a la población en general el daño medioambiental que está teniendo nuestro ecosistema, a su misma vez, desarrollar mecanismos, para reducir el deterioro medioambiental, los cuales las grandes industrias son responsables, tales como transporte, sector energético, industria textil y la construcción</p>
+            <a href="#" class="btn"> Ver mas<i class="fas fa-angle-double-right"></i></a>
+          </div>
+
+        </section>
+
+    <div>    
+
+
+
+</body>
+</html>
