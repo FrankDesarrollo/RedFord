f81670fb852edd402ec04ef61dd61fa50cfeb6fd
josearpa123 <116037142+josearpa123@users.noreply.github.com>
2022-11-22 14:10:43 -0500

    Create mainpaginaprincipal.js

---
+++ mainpaginaprincipal.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-carsd', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
